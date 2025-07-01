4c496daade9ba4ecfeb7a41a71c8025568263f75
apushkarev <apushkarev@users.noreply.github.com>
2025-07-01 15:38:19 +0300

    Update: обновление регулярок в control_server

--- control_server/routers/index.js
+++ control_server/routers/index.js
@@ -1,26 +1,30 @@
 import { APIController, VRController }  from '../controllers/index.js'
 
 const routes = [
-        {
-            regex:          /^\/api\/.websocket$/,
-            controller:     APIController
-        },
-        {
-            regex:          /^\/.websocket$/,
-            controller:     VRController
-        }
+  {
+    regex: /\/api\/.websocket$/,
+    controller: APIController
+  },
+  {
+    regex: /\/.websocket$/,
+    controller: VRController
+  }
 ]
 
 const router = (ws, req) => {
-        console.log(`Request from ${req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress}`)
-        console.log(`Headers: ${JSON.stringify(req.headers)}`)
 
-        routes.forEach((route) => {
-            if (route.regex.test(req.url)) {
-                console.log(req.url)
-                route.controller(ws, req)
-            }
-        })
+    // console.log(`Request from ${req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress}`)
+    // console.log(`Headers: ${JSON.stringify(req.headers)}`)
+
+    // console.log('request url', req.url);
+
+    routes.forEach((route) => {
+
+      if (route.regex.test(req.url)) {
+        // console.log(req.url)
+        route.controller(ws, req)
+      }
+    })
 }
 
 export default router
